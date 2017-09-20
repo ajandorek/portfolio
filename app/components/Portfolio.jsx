fc2a05e0075291d0cbe7db79f99d5795384018f6
Alec Jandorek <ajandorek@gmail.com>
2017-09-19 22:35:45 -0500

    Start reformat of portfolio page

--- app/components/Portfolio.jsx
+++ app/components/Portfolio.jsx
@@ -5,26 +5,28 @@ var Portfolio = () => {
         <div>
             <h1 className='text-center page-title'>Portfolio</h1>
             <h4 className='text-center'>A collection of projects I have created or collaborated on</h4>
-            <div className="callout">
-                <h4 className='text-center'>Fallout Hangman</h4>
-                <img src={'http://i.imgur.com/2oyhTTb.png'} />
-                <p>A simple hangman game with a Fallout Theme.</p>
-                <h5>Technologies Used</h5>
-                <ul>
-                    <li>Javascript</li>
-                </ul>
-                <a className='hollow button primary' href="https://fallout-hangman.herokuapp.com/" target="_blank">Click Here</a>
-            </div>
-            <div className="callout">
-                <h4 className='text-center'>Overwatch RPG</h4>
-                <img src={'http://i.imgur.com/qbBRC4d.png'} />
-                <p>A role playing game with an overwatch spin.</p>
-                <h5>Technologies Used</h5>
-                <ul>
-                    <li>jQuery</li>
-                    <li>jQuery UI</li>
-                </ul>
-                <a className='hollow button primary' href="https://overwatch-rpg.herokuapp.com/" target="_blank">Click Here</a>
+            <div className="grid-x grid-margin-x">
+                <div className="small-4 large-offset 2 cell">
+                    <a className="callout" href="https://fallout-hangman.herokuapp.com/" 
+                    target="blank">
+                        <h4 className='text-center'>Fallout Hangman</h4>
+                        <img src={'http://i.imgur.com/2oyhTTb.png'} />
+                        <p className='text-center'>A simple hangman game with a Fallout Theme.</p>
+                        <h5 className='text-center'>Technologies Used</h5>
+                        <p className='text-center'>Javascript</p>
+                    </a>
+                </div>
+                <div className="small-4 cell">
+                    <a className="callout" href="https://overwatch-rpg.herokuapp.com/"
+                    target="blank">
+                        <h4 className='text-center'>Overwatch RPG</h4>
+                        <img src={'http://i.imgur.com/qbBRC4d.png'} />
+                        <p className='text-center'>A role playing game with an Overwatch spin.</p>
+                        <h5 className='text-center'>Technologies Used</h5>
+                        <p className='text-center'>jQuery</p>
+                        <p className='text-center'>jQuery UI</p>
+                    </a>
+                </div>
             </div>
             <div className="callout">
                 <h4 className='text-center'>Map Fun</h4>
