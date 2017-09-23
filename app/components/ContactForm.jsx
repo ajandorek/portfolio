136959a8b036329b43486f38809c1e66fa39b353
Alec Jandorek <ajandorek@gmail.com>
2017-09-22 22:40:30 -0500

    form skeleton and styling added

--- app/components/ContactForm.jsx
+++ app/components/ContactForm.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 
 var ContactForm = () => {
     return (
-        <div>
+        <div className='grid-container'>
             <p>Interested in chatting? Please feel free to contact me below!</p>
-            <form>
-                <label>Name</label>
-                <input />
-                <label>Email</label>
-                <input />
-                <label>Subject</label>
-                <input />
-                <label>Message</label>
-                <textarea />
-                <button className='button hollow'>Submit</button>
-            </form>
+            <div className="container__form">
+                <form className='input-group-rounded'>
+                    <label>Name</label>
+                    <input className='input-group-field'/>
+                    <label>Email</label>
+                    <input className='input-group-field'/>
+                    <label>Subject</label>
+                    <input className='input-group-field'/>
+                    <label>Message</label>
+                    <textarea className='input-group-field'/>
+                    <button className='button hollow'>Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
